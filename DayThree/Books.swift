b7419c15841c0ee21f9d3ed26f85786e4885296c
bmnaufaal <bmuhammadopang@gmail.com>
2022-06-15 15:40:26 +0700

    Add Podfile

---
+++ DayThree/Books.swift
@@ -0,0 +1,14 @@
+//
+//  Books.swift
+//  DayThree
+//
+//  Created by Berlian on 15/06/22.
+//
+
+import Foundation
+
+struct Books : Decodable {
+    let title: String
+    let author: String
+    let year: Int
+}
