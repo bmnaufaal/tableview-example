263284997e69c994b79d3663cd307ac7cd47d58b
bmnaufaal <bmuhammadopang@gmail.com>
2022-06-15 09:35:11 +0700

    first commit

---
+++ DayThree/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DayThree
+//
+//  Created by Berlian on 15/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyPlayground/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  DayThree
+//
+//  Created by Berlian on 15/06/22.
+//
+
+import Foundation
+
